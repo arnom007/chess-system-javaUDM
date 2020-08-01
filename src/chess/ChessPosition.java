cb7dd77aef68ab456e7f095e40f68b3aa5e7ac25
arnom007 <arnomthg@gmail.com>
2020-08-01 15:07:16 -0300

    Little improvement in board printing

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,8 +8,8 @@ public class ChessPosition {
 	private char column;
 
 	public ChessPosition(char column, int row) {
-		if ((column < 'a' || column > 'h') || (row < 0 || row > 7))
-			throw new ChessException("Invalid chess position.");
+		if ((column < 'a' || column > 'h') || (row < 0 || row >8))
+			throw new ChessException("Invalid chess position. Valid values are from a1 to h8.");
 		this.column = column;
 		this.row = row;
 	}
@@ -25,9 +25,9 @@ public class ChessPosition {
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
-
+	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
 	}
 
 	@Override
