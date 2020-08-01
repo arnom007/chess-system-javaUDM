f3bbb16df6596500b7542ce5688da535c6f2b822
arnom007 <arnomthg@gmail.com>
2020-08-01 14:35:03 -0300

    ChessException and ChessPosition

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,11 +4,11 @@ public class Position {
 
 	private int row;
 	private int column;
-	
-	public Position(){
-		
-	}	
-	
+
+	public Position() {
+
+	}
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
@@ -17,19 +17,22 @@ public class Position {
 	public int getRow() {
 		return row;
 	}
+
 	public void setRow(int row) {
 		this.row = row;
 	}
+
 	public int getColumn() {
 		return column;
 	}
+
 	public void setColumn(int column) {
 		this.column = column;
 	}
-	
+
 	@Override
 	public String toString() {
 		return row + ", " + column;
 	}
-	
+
 }
